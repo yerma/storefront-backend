cbdacc017f6c7279e2cf6f155e6825e82043c50b
Yerma <yerma.araya@gmail.com>
2021-08-06 21:58:14 -0400

    chore: initial setup

---
+++ src/database.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config()
+
+const {
+  NODE_ENV,
+  POSTGRES_HOST: host,
+  POSTGRES_DB: database,
+  POSTGRES_DB_TEST: testDatabase,
+  POSTGRES_USER: user,
+  POSTGRES_PASSWORD: password,
+} = process.env;
+
+let client: Pool;
+
+if (NODE_ENV === 'development') {
+  client = new Pool({ host, database, user, password })
+} else if (NODE_ENV === 'test') {
+  client = new Pool({ host, user, password, database: testDatabase })
+} else {
+  throw new Error('DB Pool not initialized.')
+}
+
+export default client;
