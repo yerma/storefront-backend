d990536fbbc3e8835f95fbf1767ac060e623c229
Yermain Araya <yerma.araya@gmail.com>
2021-09-12 20:25:59 -0300

    feat: updated server with handlers and cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import cors, { CorsOptions } from 'cors'
 import productRoutes from './handlers/products'
 import userRoutes from './handlers/users'
+import orderRoutes from './handlers/orders'
+import dashboardRoutes from './handlers/dashboard'
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
 
+const corsOptions: CorsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
 
 app.get('/', function (req: Request, res: Response) {
@@ -14,6 +23,8 @@ app.get('/', function (req: Request, res: Response) {
 
 productRoutes(app)
 userRoutes(app)
+orderRoutes(app)
+dashboardRoutes(app)
 
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`)
