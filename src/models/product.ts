9e9af9e87d2aa2ba5684de9b85989164e1adf946
Yerma <yerma.araya@gmail.com>
2021-09-13 23:38:10 -0300

    test: added product tests

--- src/models/product.ts
+++ src/models/product.ts
@@ -36,7 +36,7 @@ export class ProductStore {
     try {
       const { name, price, category } = product;
       const conn = await Client.connect();
-      const sql = 'INSERT INTO products (name, price, category) VALUES ($1, $2, $3);'
+      const sql = 'INSERT INTO products (name, price, category) VALUES ($1, $2, $3) RETURNING *;'
       const newProduct = await conn.query(sql, [name, price, category])
       conn.release()
       return newProduct.rows[0] as unknown as Product
@@ -49,7 +49,7 @@ export class ProductStore {
     try {
       const { name, price, category } = product
       const conn = await Client.connect();
-      const sql = 'UPDATE products SET name = ($2), price = ($3), category = ($4) WHERE id = ($1);'
+      const sql = 'UPDATE products SET name = ($2), price = ($3), category = ($4) WHERE id = ($1) RETURNING *;'
       const updatedProduct = await conn.query(sql, [id, name, price, category])
       conn.release()
       return updatedProduct.rows[0] as unknown as Product
@@ -61,7 +61,7 @@ export class ProductStore {
   async delete(id: string): Promise<Product> {
     try {
       const conn = await Client.connect();
-      const sql = 'DELETE FROM products WHERE id = ($1);'
+      const sql = 'DELETE FROM products WHERE id = ($1) RETURNING *;'
       const deletedProduct = await conn.query(sql, [id])
       conn.release()
       return deletedProduct.rows[0] as unknown as Product
---
+++ src/models/tests/productSpec.ts
@@ -0,0 +1,36 @@
+import { Product, ProductStore } from '../product'
+
+const store = new ProductStore()
+
+describe('Product Model', () => {
+  let product: Product;
+
+  it('create method should create a product', async () => {
+    product = await store.create({
+      name: 'Shoes',
+      price: 50,
+      category: 'footwear'
+    })
+    expect(product).toBeDefined()
+  })
+
+  it('index method should return empty array ', async () => {
+    const result = await store.index()
+    expect(result.length).toEqual(1)
+  })
+
+  it('show method should return product with provided id', async () => {
+    const showProduct = await store.show(product.id as string)
+    expect(showProduct).toEqual(product)
+  })
+
+  it('edit method should update product with provided id', async () => {
+    const edited = await store.edit(product.id as string, {...product, price: 60})
+    expect(edited.price).not.toEqual(product.price)
+  })
+
+  it('delete method should delete product with provided id', async () => {
+    const deleted = await store.delete(product.id as string)
+    expect(deleted).toBeDefined()
+  })
+})
