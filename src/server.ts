9426c1cd21c724674ab64fbf195e979604146257
Yermain Araya <yerma.araya@gmail.com>
2021-10-02 00:18:05 -0300

    fix: use express json instead of bodyparser

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response, json } from 'express'
 import cors, { CorsOptions } from 'cors'
 import productRoutes from './handlers/products'
 import userRoutes from './handlers/users'
@@ -15,7 +14,7 @@ const corsOptions: CorsOptions = {
 }
 
 app.use(cors(corsOptions))
-app.use(bodyParser.json())
+app.use(json())
 
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
